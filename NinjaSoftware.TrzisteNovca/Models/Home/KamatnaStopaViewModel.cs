102127308bbfce7375ec0581f953dfad29bd619e
ninja <mamasic@gmail.com>
2013-05-06 14:21:58 +0200

    Sudionik sidepicture

---
+++ NinjaSoftware.TrzisteNovca/Models/Home/KamatnaStopaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NinjaSoftware.TrzisteNovca.CoolJ.DatabaseGeneric.BusinessLogic;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace NinjaSoftware.TrzisteNovca.Models.Home
+{
+    public class KamatnaStopaViewModel
+    {
+        public KamatnaStopaViewModel(DataAccessAdapterBase adapter, bool jeMedubankarskoTrgovanje, long trgovanjeId)
+        { }
+
+        public Dictionary<TrgovanjeVrstaEnum, decimal?> TrgovanjeKamatnaStopaDictionary { get; set; }
+        public DateTime Datum { get; set; }
+        public bool JeMedubankarskoTrgovanje { get; set; }
+    }
+}
